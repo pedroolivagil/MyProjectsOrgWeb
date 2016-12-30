62193f1831601f68f47fe5be0bc60b0afaf96630
pedroolivagil <onion_oliva@hotmail.com>
2016-12-30 13:03:48 +0100

    V2.0.0.2 Añadido paises

--- php/project_manager/paises.php
+++ php/project_manager/paises.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('Tools.php');
-require_once('db/Database.php');
+require_once('../Tools.php');
+require_once('../db/Database.php');
 
 header('Content-type: application/json');
 Database::init_db();
