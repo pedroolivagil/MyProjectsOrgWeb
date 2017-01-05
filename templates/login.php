c95268c652c96e7340a8cbcd2a2c48d310c51fa6
0013856 <0013856@S4C66603.insags.com>
2017-01-05 13:41:23 +0100

    Pantalla de Login y maquetacion

--- templates/login.php
+++ templates/login.php
@@ -1,33 +1,33 @@
 <!--// BreadCrumbs //-->
 <ol class="breadcrumb shadow translucid-80 width100">
-    <li><a href="#"><?php $translator->getText('HOME_PAGE'); ?></a></li>
-    <li class="active"><?php $translator->getText('LOGIN_PAGE'); ?></li>
+    <li><a href="#"><?php echo $translator->getText('HOME_PAGE'); ?></a></li>
+    <li class="active"><?php echo $translator->getText('LOGIN_PAGE'); ?></li>
 </ol>
 
 <!--// Content //-->
 <div class="container container-login">
     <div class="panel panel-primary">
         <div class="panel-heading">
-            <h4><?php $translator->getText('LOGIN_PAGE_TITLE'); ?></h4>
+            <h4><?php echo $translator->getText('LOGIN_PAGE_TITLE'); ?></h4>
         </div>
         <div class="panel-body">
             <form class="form-inline" role="form">
                 <div class="form-group width100">
-                    <label class="sr-only" for="login_email"><?php $translator->getText('LOGIN_PAGE_EMAIL'); ?></label>
-                    <input type="email" class="form-control width100" id="login_email" placeholder="<?php $translator->getText('LOGIN_PAGE_PLACEHOLDER_EMAIL'); ?>">
+                    <label class="sr-only" for="login_email"><?php echo $translator->getText('LOGIN_PAGE_EMAIL'); ?></label>
+                    <input type="email" class="form-control width100" id="login_email" placeholder="<?php echo $translator->getText('LOGIN_PAGE_PLACEHOLDER_EMAIL'); ?>">
                 </div>
                 <div class="form-group pad5-tb width100">
-                    <label class="sr-only" for="password_login"><?php $translator->getText('LOGIN_PAGE_PASSWORD'); ?></label>
-                    <input type="password" class="form-control width100" id="password_login" placeholder="<?php $translator->getText('LOGIN_PAGE_PLACEHOLDER_PASS'); ?>">
+                    <label class="sr-only" for="password_login"><?php echo $translator->getText('LOGIN_PAGE_PASSWORD'); ?></label>
+                    <input type="password" class="form-control width100" id="password_login" placeholder="<?php echo $translator->getText('LOGIN_PAGE_PLACEHOLDER_PASS'); ?>">
                 </div>
                 <div class="checkbox pad5-tb hidden">
                     <label>
-                        <input type="checkbox"> <?php $translator->getText('LOGIN_PAGE_AUTOLOGIN_TEXT'); ?>
+                        <input type="checkbox"> <?php echo $translator->getText('LOGIN_PAGE_AUTOLOGIN_TEXT'); ?>
                     </label>
                 </div>
-                <button type="submit" class="btn btn-primary width100 mar5-tb"><?php $translator->getText('LOGIN_PAGE_SIGN_IN'); ?></button>
+                <button type="submit" class="btn btn-primary width100 mar5-tb"><?php echo $translator->getText('LOGIN_PAGE_SIGN_IN'); ?></button>
                 <hr>
-                <a class="btn btn-success width100 mar5-tb"><?php $translator->getText('LOGIN_PAGE_SIGN_UP'); ?></a>
+                <a class="btn btn-success width100 mar5-tb"><?php echo $translator->getText('LOGIN_PAGE_SIGN_UP'); ?></a>
             </form>
         </div>
     </div>
--- templates/signup.php
+++ templates/signup.php
@@ -1,33 +1,30 @@
 <!--// BreadCrumbs //-->
 <ol class="breadcrumb shadow translucid-80 width100">
-    <li><a href="#"><?php $translator->getText('HOME_PAGE'); ?></a></li>
-    <li class="active"><?php $translator->getText('LOGIN_PAGE'); ?></li>
+    <li><a href="#"><?php echo $translator->getText('HOME_PAGE'); ?></a></li>
+    <li class="active"><?php echo $translator->getText('SIGN_UP_PAGE'); ?></li>
 </ol>
 
 <!--// Content //-->
-<div class="container container-login">
+<div class="container container-signup">
     <div class="panel panel-primary">
         <div class="panel-heading">
-            <h4><?php $translator->getText('LOGIN_PAGE_TITLE'); ?></h4>
+            <h4><?php echo $translator->getText('SIGN_UP_PAGE_TITLE'); ?></h4>
         </div>
         <div class="panel-body">
             <form class="form-inline" role="form">
                 <div class="form-group width100">
-                    <label class="sr-only" for="login_email"><?php $translator->getText('LOGIN_PAGE_EMAIL'); ?></label>
-                    <input type="email" class="form-control width100" id="login_email" placeholder="<?php $translator->getText('LOGIN_PAGE_PLACEHOLDER_EMAIL'); ?>">
+                    <label class="sr-only" for="signup_"><?php echo $translator->getText('SIGN_UP_PAGE_'); ?></label>
+                    <input type="" class="form-control width100" id="signup_" placeholder="<?php echo $translator->getText('SIGN_UP_PAGE_PLACEHOLDER_'); ?>">
                 </div>
-                <div class="form-group pad5-tb width100">
-                    <label class="sr-only" for="password_login"><?php $translator->getText('LOGIN_PAGE_PASSWORD'); ?></label>
-                    <input type="password" class="form-control width100" id="password_login" placeholder="<?php $translator->getText('LOGIN_PAGE_PLACEHOLDER_PASS'); ?>">
-                </div>
-                <div class="checkbox pad5-tb hidden">
+                
+                <div class="checkbox pad5-tb">
                     <label>
-                        <input type="checkbox"> <?php $translator->getText('LOGIN_PAGE_AUTOLOGIN_TEXT'); ?>
+                        <input type="checkbox"> <?php echo $translator->getText('SIGN_UP_PAGE_TERMS'); ?>
                     </label>
                 </div>
-                <button type="submit" class="btn btn-primary width100 mar5-tb"><?php $translator->getText('LOGIN_PAGE_SIGN_IN'); ?></button>
+                <button type="submit" class="btn btn-primary width100 mar5-tb"><?php echo $translator->getText('SIGN_UP_PAGE_SIGNUP'); ?></button>
                 <hr>
-                <a class="btn btn-success width100 mar5-tb"><?php $translator->getText('LOGIN_PAGE_SIGN_UP'); ?></a>
+                <a class="btn btn-danger width100 mar5-tb"><?php echo $translator->getText('GENERIC_CANCEL'); ?></a>
             </form>
         </div>
     </div>
