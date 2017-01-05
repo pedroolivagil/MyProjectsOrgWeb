3e17edb2b4e94962d943e435b13cfbbffa711427
0013856 <0013856@S4C66603.insags.com>
2017-01-05 10:16:53 +0100

    Implementación Bootsrap 3

---
+++ html/login.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <ol class="breadcrumb">
+        <li><a href="#"><?php $translator->getText('HOME_PAGE'); ?></a></li>
+        <li class="active"><a href="#"><?php $translator->getText('LOGIN_PAGE'); ?></a></li>
+    </ol>
+
+    <p>Formulario de login</p>
+</div>
