d147cd60c9a528b80de1523525e25233c22382e1
0013856 <0013856@S4C66603.insags.com>
2017-01-09 17:26:15 +0100

    Pantalla de Login, SignUp y maquetacion

--- php/User.php
+++ php/User.php
@@ -30,7 +30,92 @@ class User {
     }
 
     public function create() {
+        //Database::begin_trans();
+        
+        //Database::insert($arrayFieldsValues, TABLE_USUARIO);
+        var_dump(serialize($this));
+    }
+
+    public function update(Usuario $usuario) {
         
     }
+    
+    public function delete(Usuario $usuario){
+        
+    }
+            
+    function getId_usuario() {
+        return $this->id_usuario;
+    }
+
+    function getCorreo() {
+        return $this->correo;
+    }
+
+    function getUser_pass() {
+        return $this->user_pass;
+    }
+
+    function getFecha_alta() {
+        return $this->fecha_alta;
+    }
+
+    function getFlag_activo() {
+        return $this->flag_activo;
+    }
+
+    function getNif() {
+        return $this->nif;
+    }
+
+    function getTelefono() {
+        return $this->telefono;
+    }
+
+    function getId_pais() {
+        return $this->id_pais;
+    }
+
+    function getPoblacion() {
+        return $this->poblacion;
+    }
+
+    function setId_usuario($id_usuario) {
+        $this->id_usuario = $id_usuario;
+    }
+
+    function setCorreo($correo) {
+        $this->correo = $correo;
+    }
+
+    function setUser_pass($user_pass) {
+        $this->user_pass = $user_pass;
+    }
+
+    function setFecha_alta($fecha_alta) {
+        $this->fecha_alta = $fecha_alta;
+    }
+
+    function setFlag_activo($flag_activo) {
+        $this->flag_activo = $flag_activo;
+    }
+
+    function setNif($nif) {
+        $this->nif = $nif;
+    }
+
+    function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+
+    function setId_pais($id_pais) {
+        $this->id_pais = $id_pais;
+    }
+
+    function setPoblacion($poblacion) {
+        $this->poblacion = $poblacion;
+    }
+
+
 
 }
--- www/php/signup_user.php
+++ www/php/signup_user.php
@@ -4,8 +4,19 @@ require_once('../../config.php');
 error_reporting(1);
 Database::init_db();
 $finalPage = 'signup-finish';
-echo '<br />' . $user = $_POST['login_email'];
-echo '<br />' . $pass = $_POST['password_login'];
+
+echo '<br />' . $user = $_POST['signup_email'];
+echo '<br />' . Tools::encrypt($user);
+echo '<br />' . $pass = $_POST['signup_password'];
+echo '<br />' . $pass2 = $_POST['signup_password2'];
+echo '<br />' . Tools::encrypt($pass);
+echo '<br />' . $fullname = $_POST['signup_fullname'];
+echo '<br />' . $birth = $_POST['signup_birthdate'];
+echo '<br />' . $nif = $_POST['signup_nif'];
+echo '<br />' . $phone = $_POST['signup_phone'];
+echo '<br />' . $country = $_POST['signup_country'];
+echo '<br />' . $state = $_POST['signup_state'];
+
 if ($user != NULL && $pass != NULL) {
 }
 Database::close_db();
